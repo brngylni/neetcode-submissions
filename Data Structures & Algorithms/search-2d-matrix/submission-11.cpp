69cb0880b8d44f2066b4a1bdeba0f9c841067d46
Mehmet Baran Geylani <66281850+brngylni@users.noreply.github.com>
2026-09-15 09:53:21 +0300

    Add: search-2d-matrix - submission-11

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-11.cpp
@@ -0,0 +1,44 @@
+class Solution {
+   public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int left = 0;
+        int right = matrix.size() - 1;
+        int rowLen = matrix[0].size() - 1;
+
+        while (left <= right) {
+            int row = (right + left) / 2;
+
+            if (matrix[row][0] > target) {
+                right = row - 1;
+                continue;
+            } else if (matrix[row][rowLen] < target) {
+                left = row + 1;
+                continue;
+            }
+            break;
+        }
+
+        if (left > right) {
+            return false;
+        }
+
+        int rowLeft = 0;
+        int rowRight = rowLen;
+        int row = (right + left) / 2;
+        while (rowLeft <= rowRight) {
+            int tempIndex = (rowRight + rowLeft) / 2;
+            
+            int tempVal = matrix[row][tempIndex];
+
+            if (tempVal > target) {
+                rowRight = tempIndex - 1;
+                continue;
+            } else if (tempVal < target) {
+                rowLeft = tempIndex + 1;
+                continue;
+            }
+            return true;
+        }
+        return false;
+    }
+};
